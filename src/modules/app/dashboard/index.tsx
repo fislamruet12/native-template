b801d8241ddc0644ad2d8b7d7129fc555813b66e
Md faridul islam <fislam.ruet@gmail.com>
2022-06-12 11:19:00 +0600

    width is added

--- src/modules/app/dashboard/index.tsx
+++ src/modules/app/dashboard/index.tsx
@@ -3,37 +3,51 @@ import {Box, Center, Flex, Heading, Pressable, Text, VStack} from 'native-base';
 import {Image, ScrollView} from 'react-native';
 import {icons} from '../../../assets/icons';
 import {bloodGroup} from '../../../utils/blood';
-import { width } from '../../../utils/handy';
-import { APP_NAVIGATION, AUTH_NAVIGATION, ROOT_NAVIGATION } from '../../../../typings/navigation';
+import {width} from '../../../utils/handy';
+import {
+  APP_NAVIGATION,
+  AUTH_NAVIGATION,
+  ROOT_NAVIGATION,
+} from '../../../../typings/navigation';
 import Navigation from '../..';
-
-const DashBoardScreen = (props:any) => {
-    const privateRoute=()=>{
-     props.navigation.push(ROOT_NAVIGATION.AUTH)
-
-    }
-    const donorList=()=>{
-        privateRoute()
-    }
+import _ from 'lodash';
+const DashBoardScreen = (props: any) => {
+  const privateRoute = () => {
+    props.navigation.push(ROOT_NAVIGATION.AUTH);
+  };
+  const donorList = () => {
+    privateRoute();
+  };
   return (
-    <Box flex={1} safeAreaTop bg='white' alignItems={'center'}>
+    <Box flex={1} safeAreaTop bg="white" alignItems={'center'}>
       <ScrollView>
-        <VStack width="100%" px="3" space={2.5} mt="4">
+        <VStack width={width} px="3" space={2.5} mt="4">
           <Flex direction="row" mb="2.5" _text={{color: 'coolGray.800'}}>
-            <Pressable onPress={() => donorList()}>
-              <Center rounded={'xl'} size="xl" bg="primary.100">
+            <Pressable onPress={_.debounce(() => donorList(), 200)}>
+              <Center rounded={'xl'} size={width / 3 - 10} bg="primary.100">
                 <Image source={icons.blooddonor} />
                 <Text bold>Donor List</Text>
               </Center>
             </Pressable>
-            <Pressable onPress={() =>  props.navigation.push(APP_NAVIGATION.SEARCHNDONOR)}>
-              <Center mx={1} rounded={'xl'} size="xl" bg="primary.200">
-                <Image source={icons.searchdonor} style={{width: 30, height: 30}} />
+            <Pressable
+              onPress={_.debounce(
+                () => props.navigation.push(APP_NAVIGATION.SEARCHNDONOR),
+                200,
+              )}>
+              <Center
+                mx={1}
+                rounded={'xl'}
+                size={width / 3 - 10}
+                bg="primary.200">
+                <Image
+                  source={icons.searchdonor}
+                  style={{width: 30, height: 30}}
+                />
                 <Text bold>Search Donor</Text>
               </Center>
             </Pressable>
             <Pressable onPress={() => console.log('press')}>
-              <Center rounded={'xl'} size="xl" bg="primary.300">
+              <Center rounded={'xl'} size={width / 3 - 10} bg="primary.300">
                 <Image source={icons.blood} style={{width: 30, height: 30}} />
                 <Text bold>Blood Request</Text>
               </Center>
@@ -41,31 +55,42 @@ const DashBoardScreen = (props:any) => {
           </Flex>
           <Flex direction="row" mb="2.5">
             <Pressable onPress={() => console.log('press')}>
-              <Center rounded={'xl'} size="xl" bg="primary.100">
+              <Center rounded={'xl'} size={width / 3 - 10} bg="primary.100">
                 <Image source={icons.donor} style={{width: 30, height: 30}} />
                 <Text bold>Active Donor</Text>
               </Center>
             </Pressable>
             <Pressable onPress={() => console.log('press')}>
-              <Center mx={1} rounded={'xl'} size="xl" bg="primary.200">
+              <Center
+                mx={1}
+                rounded={'xl'}
+                size={width / 3 - 10}
+                bg="primary.200">
                 <Image source={icons.bank} style={{width: 30, height: 30}} />
                 <Text bold>Blood Bank</Text>
               </Center>
             </Pressable>
             <Pressable onPress={() => console.log('press')}>
-              <Center rounded={'xl'} size="xl" bg="primary.300">
-                <Image source={icons.update} style={{tintColor:'red'}} />
+              <Center rounded={'xl'} size={width / 3 - 10} bg="primary.300">
+                <Image source={icons.update} style={{tintColor: 'red'}} />
                 <Text bold>Update Donor</Text>
               </Center>
             </Pressable>
           </Flex>
         </VStack>
-        <VStack  px="3" space={2.5} mt="5" alignSelf={'center'} bg="primary.100" padding={'2.5'} rounded="xl">
+        <VStack
+          px="3"
+          space={2.5}
+          mt="5"
+          alignSelf={'center'}
+          bg="primary.100"
+          padding={'2.5'}
+          rounded="xl">
           <Heading size={'md'}>Nearest Blood Group</Heading>
           <Flex width="100%" direction="row" mb="2.5" flexWrap="wrap">
             {bloodGroup.map(value => (
               <Pressable onPress={() => console.log('press blood')} key={value}>
-                <Center rounded={'xl'} size="md" width={width/5} bg="primary.300" m={1} mt={4}>
+                <Center rounded={'xl'} size="md" bg="primary.300" m={1} mt={4}>
                   <Text bold>{value}</Text>
                 </Center>
               </Pressable>
